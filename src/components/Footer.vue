b6913c79e7b57b01a2fac8f45c1ae0a340dd049d
Kevin Krummnacker <kk@dogado.de>
2021-11-23 18:17:45 +0100

    feat: add Login from boilerplate-spa

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,17 +20,5 @@ const toggleLocales = () => {
       <carbon-moon v-if="isDark" />
       <carbon-sun v-else />
     </button>
-
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <carbon-language />
-    </a>
-
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <carbon-dicom-overlay />
-    </router-link>
-
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <carbon-logo-github />
-    </a>
   </nav>
 </template>
--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -2,17 +2,21 @@
   <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
-        <h2 class="mt-6 text-center text-3xl font-bold text-gray-900">
+        <h2 class="mt-6 text-center text-3xl font-bold text-gray-900 dark:text-white">
           Login
         </h2>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <div v-if="error">
+        {{ error }}
+      </div>
+      <form class="mt-8 space-y-6" method="POST" @submit.prevent="">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
             <input
               id="email-address"
+              v-model="form.email"
               name="email"
               type="email"
               autocomplete="email"
@@ -25,6 +29,7 @@
             <label for="password" class="sr-only">Password</label>
             <input
               id="password"
+              v-model="form.password"
               name="password"
               type="password"
               autocomplete="current-password"
@@ -38,14 +43,14 @@
         <div class="flex items-center justify-between">
           <div class="flex items-center">
             <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900 dark:text-white">
               Remember me
             </label>
           </div>
         </div>
 
         <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" @click="submit">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <!-- Heroicon name: solid/lock-closed -->
               <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -61,12 +66,61 @@
 </template>
 
 <script lang="ts">
+import axios from 'axios'
 import { useAuthStore } from '~/stores/auth'
 
 export default {
   setup() {
-	  const user = useAuthStore()
-    return {}
+    const router = useRouter()
+    const route = useRoute()
+    const user = useAuthStore()
+    const error = ref(null)
+    const form = reactive({
+      email: 'admin@localhost',
+      password: 'password',
+    })
+
+    return {
+      router,
+      route,
+      user,
+
+      error,
+      form,
+    }
+  },
+  methods: {
+    async submit() {
+      this.error = null
+
+      const url = new URL(import.meta.env.VITE_OAUTH_AUTH_SERVER)
+
+      // const response = await axios.post(`${url.toString()}oauth/token`, {
+      //   client_id: 2,
+      //   client_secret: '8HMNUdm8Gh3QW6l6eKoi41R7SB8lLJoLkqvmlLwE',
+      //   grant_type: 'password',
+      //   username: this.form.email,
+      //   password: this.form.password,
+      // }).then(() => {})
+      //   .catch((error) => {
+      //     this.error = error.message
+      //   })
+
+      const response = await axios.post(`${url.toString()}api/passport/login`, {
+        email: this.form.email,
+        password: this.form.password,
+      })
+        .then((response) => {
+          console.log(response.data)
+          this.user.setToken(response.data.token)
+          window.localStorage.setItem('accessToken', response.data.token)
+          // window.localStorage.setItem('refreshToken', response.data.refresh_token)
+          this.router.push('/')
+        })
+        .catch((error) => {
+          this.error = error.message
+        })
+    },
   },
 }
 </script>
